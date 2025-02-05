d61663e04a2c78f3b8a1769fe4e08a8975562794
Hemashree6 <hemashreets2702@gmail.com>
2025-02-05 14:31:00 +0530

    Create App.java

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,7 @@
+package com.example;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, this is a Maven-built Java application running via Jenkins!");
+    }
+}
